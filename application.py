fd0431efbd7cd15e54c0815bd49e6cb48778e06f
Sior Meir <siorm2310@gmail.com>
2020-01-26 21:26:25 +0200

    added second route, added .css files

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask,render_template
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,8 +10,22 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    """
+    This is the main functionality for the site. if logged in, the chat will appear in
+    this route.
+    """
+    return render_template("chatroom.html")
+
+@app.route("/login")
+def login():
+    """
+    Login route. if the user hasn't initiated a session, the login "landing page" will be displayed,
+    asking for a username and a chat
+    """
+    # TODO: add session handling for returning client
+    return render_template("landing_page.html")
 
 
 if __name__ == "__main__":
+    app.debug = True
     app.run()
