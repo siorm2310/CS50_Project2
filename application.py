3a3a9da2b2c2bae7a6ad899096242f982c814f97
Sior Meir <siorm2310@gmail.com>
2020-01-31 17:13:19 +0200

    created .js files

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask,render_template
+from flask import Flask, render_template, redirect,request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -16,12 +16,15 @@ def index():
     """
     return render_template("chatroom.html")
 
-@app.route("/login")
+@app.route("/login", methods=["POST","GET"])
 def login():
     """
     Login route. if the user hasn't initiated a session, the login "landing page" will be displayed,
     asking for a username and a chat
     """
+    if request.method == "POST":
+        user_to_register = request.form.get("username")
+        # TODO: verify that username isn't taken
     # TODO: add session handling for returning client
     return render_template("landing_page.html")
 
