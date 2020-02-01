4d960186cccfa16976a9d028875142a45fffd288
Sior Meir <siorm2310@gmail.com>
2020-02-01 08:50:06 +0200

    added websocket connection to .js files

--- application.py
+++ application.py
@@ -28,6 +28,7 @@ def login():
     # TODO: add session handling for returning client
     return render_template("landing_page.html")
 
+#   @socketio.on()
 
 if __name__ == "__main__":
     app.debug = True
---
+++ static/landing_page.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // connection to server
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+    socket.on("connect",() => {});
+
+    document.querySelector('')
+});
+
+
+
