114206b1eca1e57af6531dfe5c2984e4286bce2e
סיאור מאיר <siorm2310@gmail.com>
2020-03-14 22:15:22 +0200

    added socketIO event - posting a message

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect,request,flash,url_for,session
+from flask import Flask, render_template, redirect, request, flash, url_for, session 
+
 from flask_socketio import SocketIO, emit
 import os
 
@@ -8,13 +9,16 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # TODO: remove user that logged out
-active_user_list = ['momo'] # nicknames currently active and taken
+# Global variables
+active_user_list = ['momo']  # nicknames currently active and taken
+active_chatrooms = []
+messages = []  # TODO: maybe should be a dict?
 
 @app.route("/")
 def index():
     """
-    This is the main functionality for the site. if logged in, the chat will appear in
-    this route.
+    This is the main functionality for the site. if logged in,
+    the chat will appear in this route.
     """
     return render_template("chatroom.html")
 
@@ -35,9 +39,15 @@ def login():
             return redirect(url_for('chat_selection'))
     return render_template("landing_page.html")
 
-@app.route("/select_chat", methods = ["POST","GET"])
+@app.route("/select_chat", methods=["POST","GET"])
 def chat_selection():
-    return render_template("chat_selection_page.html", rooms = [])
+    return render_template("chat_selection_page.html", rooms=active_chatrooms)
+
+@socketio.on("submit message")
+def vote(data):
+    selection = data["selection"]
+    # TODO: add message to global variables
+    emit("all messages", messages, broadcast=True)
 
 if __name__ == "__main__":
     app.debug = True
